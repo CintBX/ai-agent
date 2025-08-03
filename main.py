471816b2b7fe825b7e13f0f5bf9fd97cd2580fa4
CintBX <D.Cint88@gmail.com>
2025-08-03 18:01:48 -0400

    CH1: LLMs; L6: Verbose

--- main.py
+++ main.py
@@ -1,20 +1,25 @@
 import os
+import sys
 from dotenv import load_dotenv
 from google import genai
-import sys
+from google.genai import types
 
 
 load_dotenv()
 api_key = os.environ.get("GEMINI_API_KEY")
-client = genai.Client(api_key=api_key)
 
 
 def main():
+    client = genai.Client(api_key=api_key)
+    messages = [
+        types.Content(role="user", parts=[types.Part(text=sys.argv[1])]),
+    ]
     has_prompt = len(sys.argv) > 1
+
     if has_prompt:
         response = client.models.generate_content(
             model="gemini-2.0-flash-001",
-            contents=sys.argv[1]
+            contents=messages
         )
         print(response.text)
         print("Prompt tokens:", response.usage_metadata.prompt_token_count)
