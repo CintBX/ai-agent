ffe4e302911d66e0f4618db760b1bc5583e1381c
CintBX <D.Cint88@gmail.com>
2025-08-05 15:41:01 -0400

    CH2: Functions; L3: Get File Content

--- tests.py
+++ tests.py
@@ -1,27 +1,39 @@
 from functions.get_files_info import get_files_info
+from functions.get_file_content import get_file_content
 
 
 def test():
-	result = get_files_info("calculator", ".")
-	print("Result for current directory:")
+	# # Test ./functions/get_files_info.py
+	# result = get_files_info("calculator", ".")
+	# print("Result for current directory:")
+	# print(result)
+	# print("")
+	# result = get_files_info("calculator", "pkg")
+	# print("Result for 'pkg' directory:")
+	# print(result)
+	# print("")
+	# result = get_files_info("calculator", "/bin")
+	# print("Result for '/bin' directory")
+	# print(result)
+	# print("")
+	# result = get_files_info("calculator", "../")
+	# print("Result for '../' directory")
+	# print(result)
+	# print("")
+
+	# Tests ./functions/get_file_content.py
+	result = get_file_content("calculator", "main.py")
+	print(result)
+	print("")	
+	result = get_file_content("calculator", "pkg/calculator.py")
+	print(result)
+	print("")	
+	result = get_file_content("calculator", "/bin/cat")
+	print(result)
+	print("")	
+	result = get_file_content("calculator", "pkg/does_not_exist.py")
 	print(result)
 	print("")
-
-	result = get_files_info("calculator", "pkg")
-	print("Result for 'pkg' directory:")
-	print(result)
-	print("")
-
-	result = get_files_info("calculator", "/bin")
-	print("Result for '/bin' directory")
-	print(result)
-	print("")
-
-	result = get_files_info("calculator", "../")
-	print("Result for '../' directory")
-	print(result)
-	print("")
-
 
 if __name__ == "__main__":
 	test()
