8b6e15382209296e135bdafe519600affcceaf64
CintBX <D.Cint88@gmail.com>
2025-08-07 12:22:27 -0400

    CH2: Functions; L5: Run Python

--- tests.py
+++ tests.py
@@ -1,6 +1,7 @@
 from functions.get_files_info import get_files_info
 from functions.get_file_content import get_file_content
 from functions.write_file import write_file
+from functions.run_python import run_python_file
 
 
 def test():
@@ -37,18 +38,32 @@ def test():
 	# print("")
 
 	# Tests ./functions/write_file.py
-	result = write_file("calculator", "lorem.txt", "wait, this isn't lorem ipsum")
+	# result = write_file("calculator", "lorem.txt", "wait, this isn't lorem ipsum")
+	# print(result)
+	# print("")
+	# result = write_file("calculator", "pkg/morelorem.txt", "lorem ipsum dolor sit amet")
+	# print(result)
+	# print("")
+	# result = write_file("calculator", "/tmp/temp.txt", "this should not be allowed")
+	# print(result)
+	# print("")
+
+	# Tests ./functions/run_python.py
+	result = run_python_file("calculator", "main.py")
 	print(result)
 	print("")
-
-	result = write_file("calculator", "pkg/morelorem.txt", "lorem ipsum dolor sit amet")
+	result = run_python_file("calculator", "main.py", ["3 + 5"])
 	print(result)
 	print("")
-
-	result = write_file("calculator", "/tmp/temp.txt", "this should not be allowed")
+	result = run_python_file("calculator", "tests.py")
 	print(result)
 	print("")
-
+	result = run_python_file("calculator", "../main.py")
+	print(result)
+	print("")
+	result = run_python_file("calculator", "nonexistent.py")
+	print(result)
+	print("")
 
 if __name__ == "__main__":
 	test()
