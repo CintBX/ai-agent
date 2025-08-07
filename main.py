3b2fc0820973018e0b7fc09be6d590be30095be1
CintBX <D.Cint88@gmail.com>
2025-08-07 12:52:26 -0400

    CH3: Function Calling; L1: System Prompt

--- main.py
+++ main.py
@@ -3,34 +3,49 @@ import sys
 from dotenv import load_dotenv
 from google import genai
 from google.genai import types
-
-
-load_dotenv()
-api_key = os.environ.get("GEMINI_API_KEY")
+from prompts import system_prompt
 
 
 def main():
-    client = genai.Client(api_key=api_key)
-    messages = [
-        types.Content(role="user", parts=[types.Part(text=sys.argv[1])]),
-    ]
-    has_prompt = len(sys.argv) > 1
-    has_verbose_flag = "--verbose" in sys.argv
-
-    if has_prompt:
-        response = client.models.generate_content(
-            model="gemini-2.0-flash-001",
-            contents=messages
-        )
-        if has_verbose_flag:
-            print(f"User prompt: {response.text}")
-            print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
-            print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
-            return
-        print(response.text)
-    else:
-        print("Please provide a prompt.")
+    load_dotenv()
+    
+    verbose = "--verbose" in sys.argv
+    args = []
+    for arg in sys.argv[1:]:
+        if not arg.startswith("--"):
+            args.append(arg)
+    
+    if not args:
+        print("AI Code Assitant")
+        print('\nUsage python3 main.py "your prompt here" [--verbose]')
+        print('Example: python3 main.py "How do I fix the calculator?')
         sys.exit(1)
+
+    api_key = os.environ.get("GEMINI_API_KEY")
+    client = genai.Client(api_key=api_key)
+    user_prompt = " ".join(args)
+
+    if verbose:
+        print(f"User prompt: {user_prompt}\n")
+
+    messages = [
+        types.Content(role="user", parts=[types.Part(text=user_prompt)]),
+    ]
+
+    generate_content(client, messages, verbose)
+
+
+def generate_content(client, messages, verbose):
+    response = client.models.generate_content(
+        model="gemini-2.0-flash-001",
+        contents=messages,
+        config=types.GenerateContentConfig(system_instruction=system_prompt),
+    )
+    if verbose:
+        print("Prompt tokens:", response.usage_metadata.prompt_token_count)
+        print("Response tokens:", response.usage_metadata.candidates_token_count)
+    print("Response:")
+    print(response.text)
 
 
 if __name__ == "__main__":
